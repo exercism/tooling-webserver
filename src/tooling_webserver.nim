e5b8d7ea9311c728f111f4a0bab0eae16269f8fa
ee7 <45465154+ee7@users.noreply.github.com>
2020-10-06 10:55:59 +0200

    Refactor: Remove a `cast` and tweak style (#22)
    * Style: Run `nimpretty`
    
    * Style: Use two spaces to indent
    
    * Style: `os.createDir` -> `createDir`
    
    In general, it's idiomatic to omit the module name in Nim.
    
    * Style: Prefer camelCase for variable names
    
    This is the usual naming convention. See:
    - https://nim-lang.org/docs/nep1.html
    
    * Style: Organise imports
    
    It can be nice to separate stdlib imports from Nimble imports.
    
    * Refactor: Tweak comments
    
    * Refactor: Remove a `cast`
    
    * Refactor: `c_signal` -> `posix.onSignal`
    
    This does the same thing internally, but looks a bit simpler. It also
    allows other signals to be added just by delimiting with a comma.

--- src/tooling_webserver.nim
+++ src/tooling_webserver.nim
@@ -1,64 +1,55 @@
-import jester
-import os
-import osproc
-import strformat
-import strutils
-import json
-import uuids
-import system/ansi_c
+import std/[json, os, osproc, posix, strformat, strutils]
+import pkg/[jester, uuids]
 
-type SignalHandler = proc (sign: cint) {.noconv.}
-
-const NimblePkgVersion {.strdefine}: string = "unknown"
+const NimblePkgVersion {.strdefine.}: string = "unknown"
 
 echo fmt"Exercism Local Tooling Webhook v{NimblePkgVersion}"
 
 router routes:
   post "/job":
-      # Uniq ID for this job
-    let job_id = $(genUUID())
+    # Create unique ID for this job
+    let jobId = $(genUUID())
 
     # Create dirs
-    let job_dir = fmt"/tmp/jobs/{job_id}"
-    let input_dir = fmt"{job_dir}/input/"
-    let output_dir = fmt"{job_dir}/output/"
-    os.createDir(input_dir)
-    os.createDir(output_dir)
+    let jobDir = fmt"/tmp/jobs/{jobId}"
+    let inputDir = fmt"{jobDir}/input/"
+    let outputDir = fmt"{jobDir}/output/"
+    createDir(inputDir)
+    createDir(outputDir)
 
     # Setup input
-    let zip_file = fmt"{job_dir}/files.zip"
-    let zip_data = request.params["zipped_files"]
-    writeFile(zip_file, zip_data)
-    discard execShellCmd fmt"unzip {zip_file} -d {input_dir}"
-    echo execProcess(fmt"ls -lah {input_dir}")
+    let zipFile = fmt"{jobDir}/files.zip"
+    let zipData = request.params["zipped_files"]
+    writeFile(zipFile, zipData)
+    discard execShellCmd fmt"unzip {zipFile} -d {inputDir}"
+    echo execProcess(fmt"ls -lah {inputDir}")
 
     # Run command
-    let exercise_name = request.params["exercise"]
-    let cmd = fmt"./bin/run.sh {exercise_name} {input_dir} {output_dir}"
+    let exerciseName = request.params["exercise"]
+    let cmd = fmt"./bin/run.sh {exerciseName} {inputDir} {outputDir}"
     setCurrentDir(request.params["working_directory"])
-    let exit_status = execShellCmd(cmd)
+    let exitStatus = execShellCmd(cmd)
 
+    # Construct response
     let response = %*
-        {
-            "exit_status": exit_status,
-            "output_files": %*{}
-        }
+      {
+        "exit_status": exitStatus,
+        "output_files": %*{}
+      }
 
-    for output_filepath in request.params["output_filepaths"].split(','):
-      if fileExists(fmt"{output_dir}/{output_filepath}"):
-        response["output_files"][output_filepath] = % readFile(fmt"{output_dir}/{output_filepath}")
+    for outputFilepath in request.params["output_filepaths"].split(','):
+      if fileExists(fmt"{outputDir}/{outputFilepath}"):
+        response["output_files"][outputFilepath] = % readFile(fmt"{outputDir}/{outputFilepath}")
 
     resp response
 
-proc sigTermHandler(sign: cint) =
-  quit 0
-
 proc main() =
-  c_signal(SIGTERM, cast[SignalHandler](sigTermHandler))
+  onSignal(SIGTERM):
+    quit(0)
 
   let port = Port(4567)
-  let settings = newSettings(port=port)
-  var jester = initJester(routes, settings=settings)
+  let settings = newSettings(port = port)
+  var jester = initJester(routes, settings = settings)
   jester.serve()
 
 main()
